9cc7f51e88843b481090a5900dbaed66cbc5d078
Fastly Onl <79739158+Khanh2512@users.noreply.github.com>
2022-07-25 18:13:23 +0700

    Update anime.php

--- api/anime.php
+++ api/anime.php
@@ -123,12 +123,12 @@ $tuandz = array_rand($url);
 $tuandeeptry = $url [$tuandz];
 $anime = array(
 "data" => $tuandeeptry,
-"author" => "TuanDeepTry",
+"author" => "VietKhanhApi",
 "success" => "true",
 );
 $rdimg = json_encode($anime);
 
-$i = ['\/v\/','\/','TuanDeepTry'];
-$t = ['/v/','/','TuanDeepTry'];
+$i = ['\/v\/','\/','VietKhanhApi'];
+$t = ['/v/','/','VietKhanhApi'];
 $j = str_replace($i, $t, $rdimg);
 echo $j;
