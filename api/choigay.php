eac49d4ace1716d87f8cda29109bf282a7fe2aeb
Fastly Onl <79739158+Khanh2512@users.noreply.github.com>
2022-07-26 09:42:58 +0700

    Update choigay.php

--- api/choigay.php
+++ api/choigay.php
@@ -17,12 +17,12 @@ $tuandz = array_rand($url);
 $tuandeeptry = $url [$tuandz];
 $choigay = array(
 "url" => $tuandeeptry,
-"author" => "TuanDeepTry",
+"author" => "VietKhanhApi",
 "success" => "true",
 );
 $rdimg = json_encode($choigay);
 
-$i = ['\/v\/','\/','TuanDeepTry'];
-$t = ['/v/','/','TuanDeepTry'];
+$i = ['\/v\/','\/','VietKhanhApi'];
+$t = ['/v/','/','VietKhanhApi'];
 $j = str_replace($i, $t, $rdimg);
 echo $j;
