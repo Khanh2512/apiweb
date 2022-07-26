cc3d1f2375018960702fce3790b78f0d6dd32f3e
Fastly Onl <79739158+Khanh2512@users.noreply.github.com>
2022-07-26 09:54:35 +0700

    Update sleep.php

--- api/sleep.php
+++ api/sleep.php
@@ -11,12 +11,12 @@ $tuandz = array_rand($url);
 $tuandeeptry = $url [$tuandz];
 $sleep = array(
 "data" => $tuandeeptry,
-"author" => "TuanDeepTry",
+"author" => "VietKhanhApi",
 "success" => "true",
 );
 $rdimg = json_encode($sleep);
 
-$i = ['\/v\/','\/','TuanDeepTry'];
-$t = ['/v/','/','TuanDeepTry'];
+$i = ['\/v\/','\/','VietKhanhApi'];
+$t = ['/v/','/','VietKhanhApi'];
 $j = str_replace($i, $t, $rdimg);
 echo $j;
