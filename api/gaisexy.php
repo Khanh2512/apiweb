d831babd8fca7e3d96a18c02365b0b3e9de5a503
Fastly Onl <79739158+Khanh2512@users.noreply.github.com>
2022-07-26 09:46:19 +0700

    Update gaisexy.php

--- api/gaisexy.php
+++ api/gaisexy.php
@@ -912,12 +912,12 @@ $tuandz = array_rand($url);
 $tuandeeptry = $url [$tuandz];
 $gaisexy = array(
 "data" => $tuandeeptry,
-"author" => "TuanDeepTry",
+"author" => "VietKhanhApi",
 "success" => "true",
 );
 $rdimg = json_encode($gaisexy);
 
-$i = ['\/v\/','\/','TuanDeepTry'];
-$t = ['/v/','/','TuanDeepTry'];
+$i = ['\/v\/','\/','VietKhanhApi'];
+$t = ['/v/','/','VietKhanhApi'];
 $j = str_replace($i, $t, $rdimg);
 echo $j;
