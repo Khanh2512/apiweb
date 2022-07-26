c3db2876f7d8cfe28e670a68c707d881a09fdaab
Fastly Onl <79739158+Khanh2512@users.noreply.github.com>
2022-07-26 09:50:00 +0700

    Update hug.php

--- api/hug.php
+++ api/hug.php
@@ -19,12 +19,12 @@ $tuandz = array_rand($url);
 $tuandeeptry = $url [$tuandz];
 $hug = array(
 "url" => $tuandeeptry,
-"author" => "TuanDeepTry",
+"author" => "VietKhanhApi",
 "success" => "true",
 );
 $rdimg = json_encode($hug);
 
-$i = ['\/v\/','\/','TuanDeepTry'];
-$t = ['/v/','/','TuanDeepTry'];
+$i = ['\/v\/','\/','VietKhanhApi'];
+$t = ['/v/','/','VietKhanhApi'];
 $j = str_replace($i, $t, $rdimg);
 echo $j;
