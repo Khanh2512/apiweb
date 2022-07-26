f81898804ca09fef0dd80a2fe905082d279bc023
Fastly Onl <79739158+Khanh2512@users.noreply.github.com>
2022-07-26 09:53:26 +0700

    Update punch.php

--- api/punch.php
+++ api/punch.php
@@ -20,12 +20,12 @@ $tuandz = array_rand($url);
 $tuandeeptry = $url [$tuandz];
 $punch = array(
 "url" => $tuandeeptry,
-"author" => "TuanDeepTry",
+"author" => "VietKhanhApi",
 "success" => "true",
 );
 $rdimg = json_encode($punch);
 
-$i = ['\/v\/','\/','TuanDeepTry'];
-$t = ['/v/','/','TuanDeepTry'];
+$i = ['\/v\/','\/','VietKhanhApi'];
+$t = ['/v/','/','VietKhanhApi'];
 $j = str_replace($i, $t, $rdimg);
 echo $j;
