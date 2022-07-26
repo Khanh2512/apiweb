7a8a7a1f60ccfcc3b30bd1173d27ae8c6062a865
Fastly Onl <79739158+Khanh2512@users.noreply.github.com>
2022-07-26 09:56:37 +0700

    Update videosex.php

--- api/videosex.php
+++ api/videosex.php
@@ -69,12 +69,12 @@ $tuandz = array_rand($url);
 $tuandeeptry = $url [$tuandz];
 $videosex = array(
 "url" => $tuandeeptry,
-"author" => "TuanDeepTry",
+"author" => "VietKhanhApi",
 "success" => "true",
 );
 $rdimg = json_encode($videosex);
 
-$i = ['\/v\/','\/','TuanDeepTry'];
-$t = ['/v/','/','TuanDeepTry'];
+$i = ['\/v\/','\/','VietKhanhApi'];
+$t = ['/v/','/','VietKhanhApi'];
 $j = str_replace($i, $t, $rdimg);
 echo $j;
