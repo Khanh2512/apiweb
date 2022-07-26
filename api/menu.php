b6b454d336c7195b8b5b814b21a5e989f526bc23
Fastly Onl <79739158+Khanh2512@users.noreply.github.com>
2022-07-26 09:51:51 +0700

    Update menu.php

--- api/menu.php
+++ api/menu.php
@@ -186,12 +186,12 @@ $tuandz = array_rand($url);
 $tuandeeptry = $url [$tuandz];
 $menu = array(
 "data" => $tuandeeptry,
-"author" => "TuanDeepTry",
+"author" => "VietKhanhApi",
 "success" => "true",
 );
 $rdimg = json_encode($menu , JSON_UNESCAPED_UNICODE);
 
-$i = ['\/v\/','\/','TuanDeepTry'];
-$t = ['/v/','/','TuanDeepTry'];
+$i = ['\/v\/','\/','VietKhanhApi'];
+$t = ['/v/','/','VietKhanhApi'];
 $j = str_replace($i, $t, $rdimg);
 echo $j;
