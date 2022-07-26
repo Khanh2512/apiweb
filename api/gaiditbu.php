6796b8b9fc46cd971ff76ac2acc7bac4475e1fb7
Fastly Onl <79739158+Khanh2512@users.noreply.github.com>
2022-07-26 09:45:40 +0700

    Update gaiditbu.php

--- api/gaiditbu.php
+++ api/gaiditbu.php
@@ -403,12 +403,12 @@ $tuandz = array_rand($url);
 $tuandeeptry = $url [$tuandz];
 $gaiditbu = array(
 "data" => $tuandeeptry,
-"author" => "TuanDeepTry",
+"author" => "VietKhanhApi",
 "success" => "true",
 );
 $rdimg = json_encode($gaiditbu);
 
-$i = ['\/v\/','\/','TuanDeepTry'];
-$t = ['/v/','/','TuanDeepTry'];
+$i = ['\/v\/','\/','VietKhanhApi'];
+$t = ['/v/','/','VietKhanhApi'];
 $j = str_replace($i, $t, $rdimg);
 echo $j;
