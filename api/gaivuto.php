c75192a24c20718f6eb0ee6f5266729942fde4da
Fastly Onl <79739158+Khanh2512@users.noreply.github.com>
2022-07-26 09:47:12 +0700

    Update gaivuto.php

--- api/gaivuto.php
+++ api/gaivuto.php
@@ -474,12 +474,12 @@ $tuandz = array_rand($url);
 $tuandeeptry = $url [$tuandz];
 $gaivuto = array(
 "data" => $tuandeeptry,
-"author" => "TuanDeepTry",
+"author" => "VietKhanhApi",
 "success" => "true",
 );
 $rdimg = json_encode($gaivuto);
 
-$i = ['\/v\/','\/','TuanDeepTry'];
-$t = ['/v/','/','TuanDeepTry'];
+$i = ['\/v\/','\/','VietKhanhApi'];
+$t = ['/v/','/','VietKhanhApi'];
 $j = str_replace($i, $t, $rdimg);
 echo $j;
