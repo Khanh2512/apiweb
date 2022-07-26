092f05b1c0a34c7270098c4be2096547da4648ac
Fastly Onl <79739158+Khanh2512@users.noreply.github.com>
2022-07-26 09:50:51 +0700

    Update kiss.php

--- api/kiss.php
+++ api/kiss.php
@@ -21,12 +21,12 @@ $tuandz = array_rand($url);
 $tuandeeptry = $url [$tuandz];
 $kiss = array(
 "url" => $tuandeeptry,
-"author" => "TuanDeepTry",
+"author" => "VietKhanhApi",
 "success" => "true",
 );
 $rdimg = json_encode($kiss);
 
-$i = ['\/v\/','\/','TuanDeepTry'];
-$t = ['/v/','/','TuanDeepTry'];
+$i = ['\/v\/','\/','VietKhanhApi'];
+$t = ['/v/','/','VietKhanhApi'];
 $j = str_replace($i, $t, $rdimg);
 echo $j;
